462af7ed993b5fc11086d87ecd7ba4e5b7a1e4e7
Thanatcha <bonsai.eao@gmail.com>
2019-06-30 22:44:19 +0700

    Updates

---
+++ precache-manifest.b8753475b52cc4d424567f374d42d872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb35fa993e0999e3be5f697a03926ab0",
+    "url": "/HowLongHaveYouBeenSingle/index.html"
+  },
+  {
+    "revision": "6209f7410b1e1741999a",
+    "url": "/HowLongHaveYouBeenSingle/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/HowLongHaveYouBeenSingle/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "6209f7410b1e1741999a",
+    "url": "/HowLongHaveYouBeenSingle/static/js/main.25812a5c.chunk.js"
+  },
+  {
+    "revision": "41a7e7169cf32da88202",
+    "url": "/HowLongHaveYouBeenSingle/static/js/runtime~main.7554450f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HowLongHaveYouBeenSingle/static/media/logo.5d5d9eef.svg"
+  }
+]);
